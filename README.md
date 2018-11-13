41f4e73ee3025f38559f589d463aa37d9c0bfa8e
billzhong <billzhong@gmail.com>
2018-11-13 15:44:40 -0330

    init

---
+++ README.md
@@ -0,0 +1,28 @@
+# Yupoo Downloader 又拍下载器
+
+## Requirement
+- Python 3
+- Requests
+  - ```pip install -r requirements.txt```
+
+## Usage 用法
+- 找到 sid
+  - 正常登录 [又拍](http://www.yupoo.com/account/login/) 回到主页
+  - 打开 `开发者工具` 或 `网页检查器` ，在 `Application` 或 `储存空间` 里找到 `Cookie` 里面的 `sid` 值并记下来。
+    - 或者，右键 `显示网页源代码` ，搜索 `sid:` 记下后面引号内的值。
+- 填入 sid
+  - 把上一步获取的 `sid` 值填入代码内 `SID =` 引号里面。
+    - 类似 `SID = '123defghijklmnopqrstuvw456'` 这样。
+- 生成下载列表
+  - 运行 ```python yupoo.py > urls.txt```
+    - 根据照片的数量，可能需要等一会儿。
+- 使用 `aria2` 下载
+  - 运行 ```aria2c -iurls.txt```
+  - 其他可选参数 `--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0.1 Safari/605.1.15" --header "Referer: http://www.yupoo.com/"`
+  
+### TODO
+- 在 Windows 下测试
+- 完善文档
+- 可选相册下载
+- 内置输出而不是使用输出重定向
+- 支持生成非 `aria2` 格式的列表
---
+++ yupoo.py
@@ -0,0 +1,82 @@
+import re
+
+import requests
+
+SID = ''
+
+r = requests.get('http://www.yupoo.com/account/', allow_redirects=False, cookies={
+    'sid': SID,
+})
+
+if r.status_code != 200:
+    print('wrong SID')
+    exit()
+
+api_regex = re.search(r',user: {id: (\d+),username:', r.text)
+if api_regex is None:
+    print('no UID')
+    exit()
+
+UID = api_regex.group(1)
+
+
+def get_album_photos(aid):
+    r = requests.get('http://www.yupoo.com/api/rest/', params={
+        'format': 'json',
+        'api_key': '5beaf2f754c86',
+        'ypp': 1,
+        'method': 'yupoo.albums.getPhotos',
+        'album_id': aid,
+    }, cookies={
+        'sid': SID,
+    })
+
+    data = r.json()
+
+    if data['stat'] == 'ok':
+        for photo in data['album']['photos']:
+            url, fn = get_photo_url(photo['id'])
+            print(url)
+            print('  out=' + aid + '/' + fn)
+
+
+def get_photo_url(pid):
+    r = requests.get('http://www.yupoo.com/api/rest/', params={
+        'format': 'json',
+        'api_key': '5beaf2f754c86',
+        'ypp': 1,
+        'method': 'yupoo.photos.getInfo',
+        'photo_id': pid,
+    }, cookies={
+        'sid': SID,
+    })
+
+    data = r.json()
+
+    if data['stat'] == 'ok':
+        photo = data['photo']
+        url = 'http://pic.yupoo.com/' + \
+              photo['bucket'] + '/' + photo['key'] + '/' + photo['secret'] + '.' + photo['originalformat']
+        title = photo['title'] + '.' + photo['originalformat']
+    else:
+        url = title = ''
+
+    return url, title
+
+
+r = requests.get('http://www.yupoo.com/api/rest/', params={
+    'format': 'json',
+    'api_key': '5beaf2f754c86',
+    'ypp': 1,
+    'method': 'yupoo.albums.getList',
+    'user_id': UID,
+}, cookies={
+    'sid': SID,
+})
+
+data = r.json()
+
+if data['stat'] == 'ok':
+    for album in data['albums']:
+        print('#', album['title'], album['photos'])
+        get_album_photos(album['id'])
